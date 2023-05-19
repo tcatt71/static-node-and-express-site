34c7d4b6af52a327b86a4a7dbbc7dbf02a8d69ea
Troy Coffindaffer <61867285+tcatt71@users.noreply.github.com>
2023-05-19 02:46:34 -0700

    set up middleware

--- app.js
+++ app.js
@@ -1,2 +1,7 @@
 const express = require('express');
 const projects = require('./data.json');
+
+const app = express();
+
+app.set('view engine', 'pug');
+app.set(express.static('/static', 'public'));
