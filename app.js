41f05357eedbe800c60ed92682ea82b6edf1269b
Troy Coffindaffer <61867285+tcatt71@users.noreply.github.com>
2023-05-19 04:19:56 -0700

    Set up the index route

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 const express = require('express');
-const projects = require('./data.json');
+const data = require('./data.json');
 
 const app = express();
 
 app.set('view engine', 'pug');
 app.set(express.static('/static', 'public'));
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.locals = data.projects;
+  res.render('index');
+});
