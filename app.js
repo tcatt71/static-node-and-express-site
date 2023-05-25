3fcb8797cbb43174e702f6b965a23127ae444359
Troy Coffindaffer <61867285+tcatt71@users.noreply.github.com>
2023-05-24 20:09:12 -0700

    Add code comments to app.js

--- app.js
+++ app.js
@@ -12,14 +12,18 @@ app.use(express.json());
 
 app.locals = data;
 
+
+// Homepage route
 app.get('/', (req, res) => {
   res.render('index');
 });
 
+// About me route
 app.get('/about', (req, res) => {
   res.render('about');
 });
 
+// Individual project route
 app.get('/project/:id', (req, res, next) => {
   const id = parseInt(req.params.id);
 
@@ -32,6 +36,7 @@ app.get('/project/:id', (req, res, next) => {
   }
 });
 
+// Page not found route
 app.use((req, res, next) => {
   const err = new createError.NotFound('Page cannot be found');
 
@@ -39,6 +44,7 @@ app.use((req, res, next) => {
   res.render('page-not-found');
 });
 
+// All other errors route
 app.use((err, req, res, next) => {
   err.status = (err.status || 500);
   err.message = (err.message || res.stausCode);
